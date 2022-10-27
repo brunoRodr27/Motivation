d0acfd0bc6b89c5abe206c41ce87caaaab1fa351
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-10-27 19:20:13 -0300

    Tela Inicial

---
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -0,0 +1,28 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.databinding.ActivityMainBinding
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(view: View) {
+        Toast.makeText(this,"OK", Toast.LENGTH_SHORT).show()
+    }
+}
