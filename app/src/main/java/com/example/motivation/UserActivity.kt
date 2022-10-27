b126c603d407054f9add2cafc4711e8c564457b7
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-10-27 19:57:22 -0300

    Troca de telas

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,5 +1,6 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,6 +24,16 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View) {
-        Toast.makeText(this,"OK", Toast.LENGTH_SHORT).show()
+        handleSave()
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != ""){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
     }
 }
