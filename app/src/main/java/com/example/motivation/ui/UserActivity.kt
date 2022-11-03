5a8d26d5432839275c229f93060c073172ebe4a3
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-11-03 17:41:40 -0300

    Organização

--- app/src/main/java/com/example/motivation/ui/UserActivity.kt
+++ app/src/main/java/com/example/motivation/ui/UserActivity.kt
@@ -1,11 +1,13 @@
-package com.example.motivation
+package com.example.motivation.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.motivation.databinding.ActivityMainBinding
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
 import com.example.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,16 +23,26 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonSave.setOnClickListener(this)
 
         supportActionBar?.hide()
+
+        verifyUserName()
     }
 
     override fun onClick(view: View) {
         handleSave()
     }
 
+    private fun verifyUserName() {
+        val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
+        if (name != ""){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
     private fun handleSave() {
         val name = binding.editName.text.toString()
         if (name != ""){
-            SecurityPreferences(this).storeString("USER_NAME", name)
+            SecurityPreferences(this).storeString(MotivationConstants.KEY.USER_NAME, name)
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         } else {
